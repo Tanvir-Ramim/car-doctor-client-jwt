2707977026bce7e14c7914441847265096096302
Tanvir Ramim <ramimhossan14@gmail.com>
2023-11-01 00:55:34 +0600

    interseptors

---
+++ src/hooks/UseTest.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+const UseTest = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+   return services
+};
+
+export default UseTest;
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,15 +1,11 @@
-import { useEffect, useState } from "react";
+
 import ServiceCard from "./ServiceCard";
+import UseTest from "../../../hooks/UseTest";
 
 
 const Services = () => {
-    const [services, setServices] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:5000/services')
-            .then(res => res.json())
-            .then(data => setServices(data));
-    }, [])
+     
+    const services =UseTest()
 
     return (
         <div className="mt-4">
